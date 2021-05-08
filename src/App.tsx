b87d59ceb8c351a8f0f1e0fe7454b532eca8939b
Yang Zhang <yang@users.noreply.github.com>
2021-05-07 23:51:20 -0700

    GraphiQL issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {GraphiQL} from "graphiql";
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
+
+const fetcher = createGraphiQLFetcher({
+  url: "https://swapi-graphql.netlify.app/.netlify/functions/index",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GraphiQL fetcher={fetcher}/>
     </div>
   );
 }
